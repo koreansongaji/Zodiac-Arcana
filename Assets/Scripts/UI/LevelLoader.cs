f83d5794344f1ee7bef745a3190f21c7d7a69899
waguu07 <78456934+waguu07@users.noreply.github.com>
2025-07-19 23:17:54 +0900

    Added Screen Transition
    Add LevelLoader for each scene

---
+++ Assets/Scripts/UI/LevelLoader.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private Animator _transition;
+    [SerializeField] private float _transitionTime = 5.0f;
+
+    /*private void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //LoadNextLevel();
+        //_transition.Play("Crossfade_Start");
+       
+    }*/
+    public void LoadNextLevel() => StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        Debug.Log("Starting transition");
+        _transition.SetTrigger("Start");
+        yield return new WaitForSeconds(5f);
+        Debug.Log("Loading Scene");
+        SceneManager.LoadScene(levelIndex);
+    }
+}
