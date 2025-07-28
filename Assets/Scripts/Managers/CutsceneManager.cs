3e1f4f2b18b13f5fd99920ab23e883a731a429b8
waguu07 <glitchy365@gmail.com>
2025-07-29 00:19:47 +0900

    컷신 수정

--- Assets/Scripts/Managers/CutsceneManager.cs
+++ Assets/Scripts/Managers/CutsceneManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -11,6 +12,7 @@ public class CutsceneManager : MonoBehaviour
     [SerializeField] private float _duration;
     private Coroutine _animateCoroutine;
     private int _currentScene = 1;
+    private int _finished = 0;
     private void Start()
     {
         _scene[0].alpha = 1;
@@ -23,31 +25,39 @@ public class CutsceneManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if (_currentScene < 4)
+            Debug.Log(_currentScene);
+            if (_currentScene < _scene.Count-1)
             {
                 SceneDrawer(_currentScene);
-                _currentScene++;
-                Debug.Log(_currentScene);
             }
-            else
+            else if(_finished == 1)
             {
                 _loadLevel.LoadSetLevel(1);
             }
-
+            _currentScene++;
+            
         }
     }
     private void SceneDrawer(int sceneNumber)
     {
-        if (_animateCoroutine != null)
-            StopCoroutine(_animateCoroutine);
-        _animateCoroutine = StartCoroutine(SceneLoader(_duration, sceneNumber));
+        if (sceneNumber < _scene.Count-2)
+        {
+            if (_animateCoroutine != null)
+                StopCoroutine(_animateCoroutine);
+            _animateCoroutine = StartCoroutine(SceneLoader(_duration, sceneNumber));
+        }
+        else
+        {
+            if (_animateCoroutine != null)
+                StopCoroutine(_animateCoroutine);
+            _animateCoroutine = StartCoroutine(FinalFrame(_duration, sceneNumber));
+        }
     }
     private IEnumerator SceneLoader(float duration, int sceneNumber)
     {
         float elapsed = 0f;
         while (elapsed < duration)
         {
-
             float t = elapsed / duration;
             float currValue = Mathf.Lerp(0f, 1f, t);
             _scene[sceneNumber].alpha = currValue;
@@ -56,4 +66,31 @@ public class CutsceneManager : MonoBehaviour
         }
         _scene[sceneNumber].alpha = 1;
     }
+    private IEnumerator FinalFrame(float duration, int sceneNumber)
+    {
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            float t = elapsed / duration;
+            float currValue = Mathf.Lerp(0f, 1f, t);
+            _scene[sceneNumber].alpha = currValue;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        yield return new WaitForSeconds(1);
+        _scene[sceneNumber].alpha = 1;
+
+        elapsed = 0f;
+        while (elapsed < duration)
+        {
+            float t = elapsed / duration;
+            float currValue = Mathf.Lerp(0f, 1f, t);
+            _scene[sceneNumber+1].alpha = currValue;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        _scene[sceneNumber+1].alpha = 1;
+        _finished = 1;
+    }
 }
+
